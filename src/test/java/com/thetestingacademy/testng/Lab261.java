6adbd78b893163e8f2bd9a1524d67f3c6104db43
Promode <pramoddutta_96@yahoo.com>
2024-06-20 07:23:18 +0530

    Added files for TestNG

---
+++ src/test/java/com/thetestingacademy/testng/Lab261.java
@@ -0,0 +1,27 @@
+package com.thetestingacademy.testng;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class Lab261 {
+
+    @Test
+    public void getRequest() {
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get()
+                .then().statusCode(201);
+    }
+
+    @Test
+    public void getRequest2() {
+        RestAssured
+                .given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when().get()
+                .then().statusCode(201);
+    }
+}
